d7445eb2cc0664967eea419537d02bc7b92358f5
Mohavii <159702610+Mohavii@users.noreply.github.com>
2024-04-05 00:04:19 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,21 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Fetch JSON data from file
-fetch('https://cbf67d83-7188-446b-9668-ff15d1924ba4-00-19ngter69pbcw.janeway.replit.dev/dashboard.json')
-.then(response => response.json())
-.then(data => {
-  // Process data and display it on your website
-  console.log(data);
-})
-.catch(error => console.error('Error:', error));
-            // Generate leaderboard HTML
-            var leaderboardHTML = "<ol>";
-            jsonData.forEach(player => {
-                leaderboardHTML += `<li><span>${player.nickname}</span><span>${player.score}</span></li>`;
-            });
-            leaderboardHTML += "</ol>";
+  // Fetch JSON data from file
+  fetch('https://cbf67d83-7188-446b-9668-ff15d1924ba4-00-19ngter69pbcw.janeway.replit.dev/dashboard.json')
+  .then(response => response.json())
+  .then(jsonData => {
+      // Process data and display it on your website
+      console.log(jsonData);
 
-            // Display leaderboard HTML
-            var jsonElement = document.getElementById("json-content");
-            jsonElement.innerHTML = leaderboardHTML;
-        })
-        .catch(error => console.error('Error fetching JSON:', error));
+      // Generate leaderboard HTML
+      var leaderboardHTML = "<ol>";
+      jsonData.forEach(player => {
+          leaderboardHTML += `<li><span>${player.nickname}</span><span>${player.score}</span></li>`;
+      });
+      leaderboardHTML += "</ol>";
+
+      // Display leaderboard HTML
+      var jsonElement = document.getElementById("json-content");
+      jsonElement.innerHTML = leaderboardHTML;
+  })
+  .catch(error => console.error('Error fetching JSON:', error));
+});
